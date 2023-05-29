77fb214b5bfe2462db28edd820c065d891975cb7
RotaryGrenadier <115063413+RotaryGrenadier@users.noreply.github.com>
2023-05-29 18:51:15 -0400

    make both selections work

---
+++ rps.js
@@ -0,0 +1,15 @@
+function getComputerChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+
+}
+const arr =  ['rock', 'paper', 'scissors'];
+
+function playerChoice(rps) {
+    return rps = prompt("Rock, paper, scissor SHOOT!");
+    
+}
+
+function playRound(playerSelection, computerSelection);
+
+console.log(playerChoice());
+console.log(getComputerChoice(arr));
